a5897c45e1f9742e83b0024d17abafc130e4a3e9
Josh Owens <joshua.owens@gmail.com>
2012-08-23 19:40:32 -0400

    Add some nice methods to the status model, with full tests.

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,2 +1,18 @@
 class Status < ActiveRecord::Base
+  attr_protected :up
+
+  validates :up, :inclusion => {:in => [true, false], :message => "status can't be blank"}
+
+  def status
+    up ? "up" : "down"
+  end
+
+  def status=(status)
+    matcher = status.to_s.downcase
+    if matcher == "up"
+      self.up = true
+    elsif matcher == "down"
+      self.up = false
+    end
+  end
 end
---
+++ spec/models/status_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe Status do
+  describe "#status" do
+    it "should return up when up is true" do
+      status = Status.new(status: "up", message: "We fixed our database!")
+      status.status.should == "up"
+    end
+
+    it "should return down when up is false" do
+      status = Status.new(status: "down", message: "We b0rked our database!")
+      status.status.should == "down"
+    end
+  end
+
+  describe "#status=" do
+    it "should set up to true if passed up" do
+      status = Status.new(status: "up", message: "We fixed our database!")
+      status.up.should be_true
+      status.should be_valid
+    end
+
+    it "should set up to false if passed down" do
+      status = Status.new(status: "down", message: "Our database is b0rked")
+      status.up.should be_false
+      puts status.errors.to_a
+      status.should be_valid
+    end
+
+    it "should not set up to anything if we get anything other than up or down for a status" do
+      status = Status.new(status: "WTFBBQ", message: "Our database is b0rked")
+      status.should_not be_valid
+    end
+  end
+end
